be6603fe594b94fe0d603516479d947bba85cc34
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-03-30 05:49:54 -0300

    feat: scale animated

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,40 +1,64 @@
-import { TouchableOpacity, TouchableOpacityProps, Text, View } from 'react-native';
+import { Pressable, PressableProps, Text } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 
-import { THEME } from '../../styles/theme';
-import { styles } from './styles';
+import { THEME } from "../../styles/theme";
+import { styles } from "./styles";
 
 const TYPE_COLORS = {
   EASY: THEME.COLORS.BRAND_LIGHT,
   HARD: THEME.COLORS.DANGER_LIGHT,
   MEDIUM: THEME.COLORS.WARNING_LIGHT,
-}
+};
 
-type Props = TouchableOpacityProps & {
+type Props = PressableProps & {
   title: string;
   isChecked?: boolean;
   type?: keyof typeof TYPE_COLORS;
-}
+};
 
-export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Props) {
-
+export function Level({
+  title,
+  type = "EASY",
+  isChecked = false,
+  ...rest
+}: Props) {
+  const scale = useSharedValue(1);
   const COLOR = TYPE_COLORS[type];
 
+  const animatedContainerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+    };
+  });
+
   return (
-    <TouchableOpacity {...rest}>
-      <View style={
-        [
+    <Pressable
+      onPressIn={() => (scale.value = 1.1)}
+      onPressOut={() => (scale.value = 1)}
+      {...rest}
+    >
+      <Animated.View
+        style={[
           styles.container,
-          { borderColor: COLOR, backgroundColor: isChecked ? COLOR : 'transparent' }
-        ]
-      }>
-        <Text style={
-          [
+          animatedContainerStyle,
+          {
+            borderColor: COLOR,
+            backgroundColor: isChecked ? COLOR : "transparent",
+          },
+        ]}
+      >
+        <Text
+          style={[
             styles.title,
-            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR }
-          ]}>
+            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR },
+          ]}
+        >
           {title}
         </Text>
-      </View>
-    </TouchableOpacity>
+      </Animated.View>
+    </Pressable>
   );
 }
